ce44ab829e78d237c0edfdea8450ed8e78732599
injashkin <injashkin@gmail.com>
2020-03-13 13:53:52 +0300

    gdhdxvx

--- server.js
+++ server.js
@@ -6,18 +6,34 @@ var mongoose = require('mongoose');
 
 var cors = require('cors');
 
+/** Подключаем пакет body-parser, который 
+позволяет получать из запроса данные форм **/
+var bodyParser = require('body-parser');
+
+/** Подключаем пакет dotenv, который делает
+доступными переменные окружения из файла env **/
+require('dotenv').config();
+
 var app = express();
 
 // Basic Configuration 
 var port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/ 
-// mongoose.connect(process.env.MONGOLAB_URI);
+/** Соединение с базой данных. Параметры подключения 
+читаются из переменной окружения MONGOLAB_URI файла .env **/
+mongoose.connect(process.env.MONGOLAB_URI, function (err) {
+  //Если при соединении с БД происходит ошибка, то выбрасывается исключение, и все дальнейшее исполнение функции прерывается
+  if (err) throw err;
+  //Если соединение с БД выполнено успешно выводится сообщение 'БД подключена'
+  console.log('БД подключена');
+});
 
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
